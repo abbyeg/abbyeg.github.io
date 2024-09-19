16c1bce412dc923eb2a3130bbd643313f2ac6c4f
Abby Grobbel <agrobbel@EWG234LT.grobbel.com>
2024-09-19 13:58:47 -0400

    adding a ton of extra stuff here

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,10 @@
 import { defineConfig } from 'astro/config';
 
-export default defineConfig({
-    site: 'https://abbyeg.github.io'
-});
+import react from '@astrojs/react';
 
+import tailwind from '@astrojs/tailwind';
+
+export default defineConfig({
+  site: 'https://abbyeg.github.io',
+  integrations: [react(), tailwind()]
+});
---
+++ src/data/blog-posts/hello-world.md
@@ -0,0 +1,9 @@
+---
+title: Hello World 👋
+publishDate: 30 Nov 2021
+description: Test blog post.
+---
+
+![Really nice illustration](/assets/blog/nice-illustration.webp)
+
+Empty post.
